3bd34c9afb3755416d0c69f4e1c6e178d4f88510
usany <ckd_qja@naver.com>
2026-03-11 19:29:52 +0900

    fix

--- app/routes/process.tsx
+++ app/routes/process.tsx
@@ -164,7 +164,8 @@ export default function Process() {
               {steps.map((step, index) => {
                 if (vehicle === "commute") {
                   const currentHour = new Date().getHours();
-                  const isNightTime = currentHour >= 18 || currentHour < 6;
+                  const currentMinute = new Date().getMinutes();
+                  const isNightTime = currentHour > 18 || currentHour < 6;
                   return (
                     <div key={index} className="flex items-center space-x-6">
                       <div className={`w-18 h-16 ${isNightTime ? 'bg-green-600' : 'bg-blue-600'} text-white rounded-md flex items-center justify-center font-semibold text-md z-10`}>
