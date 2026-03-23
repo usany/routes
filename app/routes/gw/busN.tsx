52f0ef0d29fbff3078e83e1a19e255e9ab6c7de9
charlie <ckd_qja@naver.com>
2026-03-24 03:34:53 +0900

    fix

--- app/routes/gw/busN.tsx
+++ app/routes/gw/busN.tsx
@@ -11,8 +11,8 @@ export default function BusN() {
   const location = useLocation();
   const pathname = location.pathname;
   const vehicle = pathname.slice(4, pathname.length);
-  const steps = getProcessSteps(vehicle);
-  const { busData, timeUntilNextFetch, fetchBusData } = useBusData(vehicle, getProcessSteps);
+  // const steps = getProcessSteps(vehicle);
+  // const { busData, timeUntilNextFetch, fetchBusData } = useBusData(vehicle, getProcessSteps);
   
 
   if (!vehicle) {
